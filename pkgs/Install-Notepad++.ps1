ddd86136434a7f18f3ec00df13ce438cdeac0dda
CLHdevOps <109356215+CLHdevOps@users.noreply.github.com>
2023-09-10 17:45:06 -0500

    Update Install-Notepad++.ps1

--- pkgs/Install-Notepad++.ps1
+++ pkgs/Install-Notepad++.ps1
@@ -1,15 +1,11 @@
-# Corey Hughes
-
+write-host 'AIB Customization: Install Notepad++'
 # Check if Chocolatey is installed
-write-host 'AIB Customization: Check if Chocolatey is installed'
-if ((Get-Command choco -ErrorAction SilentlyContinue) -eq $null) {
-    write-host 'AIB Customization: Install Chocolatey'
-    # Install Chocolatey
-    Set-ExecutionPolicy Bypass -Scope Process -Force;
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
-    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+if(!(Test-Path -Path "$env:ProgramData\chocolatey\choco.exe")) {
+    write-host 'Chocolatey not found, installing now'
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
 # Install Notepad++
-write-host 'AIB Customization: Install Notepad++'
-choco install notepadplusplus -y
+$chocoInstallCommand = 'choco install notepadplusplus -y'
+Invoke-Expression -Command $chocoInstallCommand
+write-host 'AIB Customization: Finished Install Notepad++'
